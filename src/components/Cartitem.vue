bb53d05402caac7e90a9053d4eee9f29c0497500
coolstory <vlask1n@yandex.ru>
2021-03-16 16:23:27 +0300

    lesson-6-7

---
+++ src/components/Cartitem.vue
@@ -0,0 +1,42 @@
+<template>
+      <div class="cart-product">     
+         <div class="border-line cart-item">
+            <img :src=this.getData[this.id].img> 
+            <p class="cart-item__name">{{ this.getData[this.id].name }}</p> 
+            <p class="cart-item__price">${{ this.getData[this.id].price }}</p>
+            <button @click="addToCart" class="cart-item__btn">Добавить в корзину</button>
+         </div>
+      </div>
+
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+   props: {
+      id: Number,
+   },
+   methods: {
+      addToCart() {
+         this.$emit( 'addToCart', this.id)
+      }
+   },
+   computed: {
+      ...mapGetters ([
+         'getData',
+         'getItemsInCart',
+      ]),
+      getItemData() {
+         return this.getData[this.id]
+      },
+      getItemsInCart() {
+         return this.getData[this.id]
+      }
+   },
+}
+</script>
+
+<style>
+
+</style>
