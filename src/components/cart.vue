e52bda2ce0f7ea574d67a067572b0d89f3e35424
coolstory <vlask1n@yandex.ru>
2021-04-22 14:51:14 +0300

    lesson-8

---
+++ src/components/cart.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="shopping-cart__card-products card-products">
+         <Cartitem 
+         v-for="id in getItemsInCart" 
+         :id="id"
+         :key="id"
+      />
+   </div>
+</template>
+
+<script>
+import { mapGetters, mapActions} from 'vuex'
+import Cartitem from './Cartitem.vue'
+export default {
+   components: {
+      Cartitem,
+   },
+   methods: {
+      ...mapActions([
+         'basketData',
+         'addToBasketItem',
+      ]),
+   },
+   computed: {
+      ...mapGetters([
+         'getItemsInCart'
+      ]),
+   },
+   created() {
+      this.basketData()
+   },
+}
+</script>
+
+<style>
+
+</style>
